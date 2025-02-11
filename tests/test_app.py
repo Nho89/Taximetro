dae1f59e73746d49234a0be4425c4d9d8822d92b
Nho89 <noa1720@gmail.com>
2025-02-11 11:17:30 +0100

    feat(tests): add test initRide

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,1 +1,16 @@
 import unittest
+from app import createTaximeter, initRide
+
+class testTaxi(unittest.TestCase):
+
+    def setUp(self):
+        self.state = createTaximeter()
+
+    def test_initRide(self):
+        state = self.state.copy()
+        initRide(state)
+        self.assertIsNotNone(state['startTime'])
+        self.assertEqual(state['currentStatus'], 'move')
+
+if __name__ == '__main__':
+    unittest.main()
