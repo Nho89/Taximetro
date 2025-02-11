a30b1a4503913e5849b0ffeeb29414565536b6ad
Nho89 <noa1720@gmail.com>
2025-02-11 08:24:41 +0100

    fix: add file logs in gitignore

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(message)s",
     handlers=[
-        logging.FileHandler("taximeter.log", mode="a", encoding="utf-8"),
+        logging.FileHandler(".taximeter.log", mode="a", encoding="utf-8"),
         logging.StreamHandler(),
     ],
 )
